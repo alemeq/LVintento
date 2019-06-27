cca6d18d7e296f0fa1c1c2582d0f402b7f164975
Alejandra <alemeque.am@gmail.com>
2019-06-27 15:49:02 -0400

    sin login sin errores

---
+++ app/app/app/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace app.ViewModels
+{
+    public class MainViewModel
+    {
+        #region ViewModels
+
+        public LandsViewModel Lands
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Lands = new LandsViewModel();
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+            {
+                return new MainViewModel();
+            }
+
+            return instance;
+        }
+        #endregion
+    }
+}
